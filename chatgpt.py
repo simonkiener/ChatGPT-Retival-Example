06711d4dc732e46ab9da93922ab01248bcbc2325
simonkiener <simonkiener@hotmail.com>
2024-08-18 14:27:45 +0200

    push example code

--- chatgpt.py
+++ chatgpt.py
@@ -1,42 +1,39 @@
 import os
 import sys
 
-import openai
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
+#import openai
+#from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain
 
 import constants
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
 PERSIST = False
-
 query = None
+
 if len(sys.argv) > 1:
   query = sys.argv[1]
 
+embedding = OpenAIEmbeddings()
+
 if PERSIST and os.path.exists("persist"):
   print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+  vectorstore = Chroma(persist_directory="persist", embedding_function=embedding)
 else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
   loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+  documents = loader.load()
+  vectorstore = Chroma.from_documents(documents, embedding, persist_directory="persist" if PERSIST else None)
+
+retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
 chain = ConversationalRetrievalChain.from_llm(
   llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  retriever=retriever,
 )
 
 chat_history = []
